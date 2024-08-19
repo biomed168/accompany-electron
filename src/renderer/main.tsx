bcdade7e7b387346d70d02ab750698bfcc22153a
congwa <cong_wa@163.com>
2024-08-19 14:58:57 +0800

    eslint

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElentBId('root')!).render(
+createRoot(document.getElement444ById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
